965e29f172d3e73e06f2acc1594b6bd6674205b5
Subigya Jyoti Panta <subigyapanta@gmail.com>
2018-08-03 22:56:59 +0545

    Ignored some files and created a separated file with different approaches to titanic solutuion

--- ensemble-style.py
+++ ensemble-style.py
@@ -195,43 +195,6 @@ submission.to_csv(path_or_buf='submissions/EnsembleSubmission.csv', index=False)
 # print('Random Forest Train: ', score_rf, "Test: ", score_rf_test)
 # print(random_forest.feature_importances_)
 
-'''
-nn = MLPClassifier((5,5), max_iter=1000, solver='adam')
-nn.fit(x_train, y_train)
-
-score_nn = nn.score(x_train, y_train)
-score_nn_test = nn.score(x_test, y_test)
-print("NN Train:", score_nn, "NN Test: ", score_rf_test)
-
-svm = SVC()
-svm.fit(x_train, y_train)
-score_svm = svm.score(x_train, y_train)
-score_svm_test = svm.score(x_test, y_test)
-print("SVM: Train", score_svm, "SVM Test:", score_svm_test)
-
-lr = RidgeClassifier()
-lr.fit(x_train, y_train)
-score_lr = lr.score(x_train, y_train)
-score_lr_test = lr.score(x_test, y_test)
-print("Ridge Regr Train:", score_lr, "Test:", score_lr_test)
-
-logistic = LogisticRegression()
-logistic.fit(x_train, y_train)
-score_logistic = logistic.score(x_train, y_train)
-score_logistic_test = logistic.score(x_test, y_test)
-print('Logistic Regr Train:', score_logistic, 'Test:', score_logistic_test)
-
-nn_search = MLPClassifier()
-nn_cv = GridSearchCV(nn_search, {
-    'hidden_layer_sizes':[(2), (2,2), (3), (3,3), (4,4), (5,3), (5,4), (5,5), (10,5)],
-    'activation': ['logistic', 'tanh', 'relu'],
-    'solver':['adam', 'sgd'],
-    # 'verbose':[5],
-    'early_stopping': [True]
-}, n_jobs=-1)
-nn_cv.fit(x_train, y_train)
-print(nn_cv.best_estimator_, nn_cv.best_score_)
-'''
 
 # plot decision boundary
 # age_min, age_max = train_data['Age'].values.min() - 1, train_data['Age'].values.max() + 1
